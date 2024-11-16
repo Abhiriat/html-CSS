55dbcf45218401ad10dfd4377194132b3471ec40
Abhishek singh <abhiramgarhia13@gmail.com>
2024-11-16 13:26:53 +0530

    Added design folder

---
+++ Design/index.js
@@ -0,0 +1,66 @@
+var options = {
+  series: [{
+    name: "Earnings in $",
+    data: [2000, 2100, 2000, 2200, 2800, 2300, 2500, 2400, 2300, 1800, 2700, 3000]
+  }],
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: 'Earnings (past 12 months)',
+    align: 'center'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], 
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+var optionsDonutChart = {
+  series: [44, 30, 15, 8], 
+  chart: {
+    height: 350,
+    type: 'donut',
+  },
+  labels: ['Academic', 'Non-academic', 'Administration', 'Others'],
+  colors: ['#00A86B', '#007FFF', '#FFD700', '#800080'],
+  title: {
+    text: 'Employees',
+    align: 'center'
+  },
+  legend: {
+    position: 'top'
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+};
+
+var chart2 = new ApexCharts(document.querySelector("#chart2"), optionsDonutChart);
+chart2.render();
